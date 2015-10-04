cad0a717bd40c30bd87e056321f83d1b23587148
Paul O'Connor <jamespauloconnor@gmail.com>
2015-10-04 13:43:58 -0400

    Added some UI code to ListViewController.  Need to add Parse code next.

--- Emojis/EmojiListViewController.swift
+++ Emojis/EmojiListViewController.swift
@@ -8,17 +8,25 @@
 
 import UIKit
 
-class EmojiListViewController: UIViewController {
+class EmojiListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel!.text = "XXXXX"
+        return cell
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
 }
